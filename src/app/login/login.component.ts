1b505d1b056584370390db65521d5a4d0ab465e2
Tomasz Odrzywolek <tomeko92@gmail.com>
2020-05-22 01:28:36 +0200

    add reading username and password from form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,10 @@ export class LoginComponent implements OnInit {
 
   loginUser(event) {
     event.preventDefault();
-    console.log(event);
+    const target = event.target;
+    const username = target.querySelector('#username').value;
+    const password = target.querySelector('#password').value;
+    console.log(username, password);
   }
 
 }
